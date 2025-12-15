18059f862d100729f09c94a56a59fe7926d04c07
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-12-15 11:22:46 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import sendRoutes from "./routes/message.route.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", sendRoutes);
+
+app.listen(PORT || 3000, () => {
+  console.log(`Server running on ${PORT}`);
+});
