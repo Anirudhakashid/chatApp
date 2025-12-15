7a9cd7072ad1798332cdf65a673a99a9d05db3ce
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-12-15 14:12:16 +0530

    deployment ready config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.route.js";
 import sendRoutes from "./routes/message.route.js";
 
@@ -9,9 +11,25 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// getting the correct directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+
+//routes
 app.use("/api/auth", authRoutes);
 app.use("/api/message", sendRoutes);
 
-app.listen(PORT || 3000, () => {
+// Serve static files in production (deplyoment) from the backend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  // For any other route, serve the index.html file
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+}
+
+app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
